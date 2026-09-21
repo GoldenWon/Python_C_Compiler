f1b635a4c34b1e81f56686c5427aef964f2fffb0
Jimmy <golden-james@att.net>
2026-09-21 00:05:05 -0400

    Added switch for options and started lexer method.

--- c_compiler.py
+++ c_compiler.py
@@ -3,6 +3,9 @@
 import sys
 import subprocess
 
+def lexer():
+    print('TODO: The damn lexer!')
+
 numArgs = len(sys.argv)
 
 if (numArgs < 2):
@@ -14,8 +17,21 @@ cPreProcFile = cSourceFile.removesuffix('c')
 
 cPreProcFile = cPreProcFile + 'i'
 
+subprocess.run(["gcc", "-E", "-P", cSourceFile, "-o", cPreProcFile])
+
 if (numArgs == 3):
     option = sys.argv[2]
-    print('You chose option ' + option)
 
-subprocess.run(["gcc", "-E", "-P", cSourceFile, "-o", cPreProcFile])
+    match option:
+        case '--lex':
+            print('TODO: Run the lexer, but stop before parsing.')
+        case '--parse':
+            print('TODO: Run the lexer and parser, but stop before assembly generation.')
+        case '--codegen':
+            print('TODO: Perform lexing, parsing, and assembly generation, but stop before code emission.')
+        case '-S':
+            print('TODO: Emit an assembly file, but do not assemble or link it.')
+        case _:
+            sys.exit('There is no such option.')
+
+lexer()
